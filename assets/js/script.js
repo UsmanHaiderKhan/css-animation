5a4452abd4c18e1e81b9d20bcd22994c1883a1bc
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2020-04-01 22:47:54 +0500

    update added Animated web page with beautifull UI

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// Full Page Scroll Animation
+
+const $body = window.opera ? (document.compatMode == 'CSS1Compat' ? $('html') : $('body')) : $('html,body'),
+    $section = $('section');
+
+var numOfPages = $section.length - 1,
+    curPage = 0,
+    scrollLock = false;
+
+function scrollPage() {
+    $(document).on('mousewheel DOMMouseScroll', function (e) {
+        if (scrollLock) return;
+        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) navigateUp();
+        else navigateDown();
+    });
+
+    $(document).on('keydown', function (e) {
+        if (scrollLock) return;
+        if (e.which === 38) navigateUp();
+        else if (e.which === 40) navigateDown();
+    });
+}
+
+function pagination() {
+    scrollLock = true;
+    $body.stop().animate({
+            scrollTop: $section.eq(curPage).offset().top,
+        },
+        1000,
+        'swing',
+        function () {
+            scrollLock = false;
+        }
+    );
+}
+
+function navigateUp() {
+    if (curPage === 0) return;
+    curPage--;
+    pagination();
+}
+
+function navigateDown() {
+    if (curPage === numOfPages) return;
+    curPage++;
+    pagination();
+}
+
+$(function () {
+    scrollPage();
+});
